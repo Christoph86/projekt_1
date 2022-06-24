bbea5af47648322aa916f41640b304d7c624876b
Christoph86 <ChrstphBch@web.de>
2022-06-24 15:31:12 +0200

    added classes(blank) like suggested for MVP, nonMVP(commented)

---
+++ src/game.js
@@ -0,0 +1,32 @@
+//MVP:
+
+//the game
+class Game {}
+
+//class for all items in the Game
+class GameItem {}
+
+//the player
+class Player extends GameItem {}
+
+//the Enemy's
+class Enemy extends GameItem {}
+
+
+
+
+
+
+
+// for later: non MVP
+//
+// class Bullet extends GameItem {}
+// //player (or maybe foes) shoting that will remove elements on collision
+// //(or decrease health later)
+
+// //a item scrolling down with the background at same speed
+// //that will block the players movement
+// class massiveItem {}
+
+// //a item just to dekorate the scrolling background
+// class backgroundItem {}
